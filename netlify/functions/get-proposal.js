0b51196de5376d79942a1a0bc5904143d805e354
Lucky783594 <pagallucky692@gmail.com>
2026-05-20 14:41:16 +0530

    updated netlify/functions/get-proposal.js

--- netlify/functions/get-proposal.js
+++ netlify/functions/get-proposal.js
@@ -20,15 +20,14 @@ exports.handler = async (event) => {
   }
 
   try {
-    // Fetch proposal
     const { data, error } = await supabase
       .from('proposals')
-      .select('id, girlfriend_name, your_name, message, theme, yes_clicked')
+      .select('id, girlfriend_name, your_name, custom_message, views, yes_clicked, created_at')
       .eq('id', id)
       .single();
 
     if (error || !data) {
-      return { statusCode: 404, headers, body: JSON.stringify({ error: 'Proposal nahi mila' }) };
+      return { statusCode: 404, headers, body: JSON.stringify({ error: 'Proposal nahi mila!' }) };
     }
 
     // Views increment (fire and forget)
